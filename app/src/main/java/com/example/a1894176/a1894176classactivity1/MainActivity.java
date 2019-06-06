ccce6141507179f3ff5b1dbc312e6d534fb7173b
Dattar <1894176@cegepgim.lcl>
2019-06-06 15:11:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1894176/a1894176classactivity1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a1894176.a1894176classactivity1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    Button btn_click;
+    btn-click;findViewById(R.id.btn_click);
+
+        btn_click.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v){
+
+
+        }
+
+
